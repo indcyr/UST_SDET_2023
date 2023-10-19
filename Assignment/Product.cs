23be90ce4fe9be939a67686b943631d85bf91c8d
indcyr <Indu.Cyriac@ust.com>
2023-10-19 17:30:22 +0530

    19-10-23

---
+++ Assignment/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Product
+    {
+        private string productName;
+        private double price, quantity;
+
+        public string ProductName { get => productName; set => productName = value; }
+        public double Price { get => price; set => price = value; }
+        public double Quantity { get => quantity; set => quantity = value; }
+
+        public Product(string productName, double price, double quantity)
+        {
+            this.ProductName = productName;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+        public void SetPrice(double newPrice)
+        {
+            Price = newPrice;
+        }
+        public double ProductValue() 
+        {
+         return Price * Quantity;
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,15 @@
 ﻿using Assignment;
 
-Student student = new Student("abc",20,18,15);
-Console.WriteLine(student.CalculateAverage());
+Product product1 = new Product("abc",20,18);
+Product product2 = new Product("xyz", 10, 10);
+Product product3 = new Product("def", 15, 20);
+
+Product[] products = {product1, product2, product3};
+
+for (int i = 0; i < products.Length; i++)
+{
+    Console.WriteLine("Product Name : " + products[i].ProductName);
+    Console.WriteLine("Price : " + products[i].Price);
+    Console.WriteLine("Quantity : " + products[i].Quantity);
+    Console.WriteLine("Total value : " + products[i].ProductValue());
+}
