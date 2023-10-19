d57379b605d3a9eb5e9f437b9d29f6fdc6cd770f
indcyr <Indu.Cyriac@ust.com>
2023-10-19 18:57:23 +0530

    19-10-23

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Assignment;
 
-Product product1 = new Product("abc",20,18);
+/*Product product1 = new Product("abc",20,18);
 Product product2 = new Product("xyz", 10, 10);
 Product product3 = new Product("def", 15, 20);
 
@@ -12,4 +12,16 @@ for (int i = 0; i < products.Length; i++)
     Console.WriteLine("Price : " + products[i].Price);
     Console.WriteLine("Quantity : " + products[i].Quantity);
     Console.WriteLine("Total value : " + products[i].ProductValue());
+}
+*/
+
+Student[] student = new Student[3];
+student[0] = new Student("XYZ",new int[] {10,20,30},"A");
+student[1] = new Student("ABC", new int[] { 12, 22, 32 }, "B");
+student[2] = new Student("DEF", new int[] { 9, 18, 11 }, "C");
+
+foreach (Student s in student)
+{
+    s.DisplayStudentInfo();
+    s.GetMarksSummary();
 }
--- Assignment/Student.cs
+++ Assignment/Student.cs
@@ -1,17 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
 
 namespace Assignment
 {
     class Student
     {
-        public string name;
-        public double grade1,grade2,grade3;
+        private string? name;
+        private int[] marks = new int[3];
+        private string grade;
 
-        public Student(string name, int grade1, int grade2, int grade3)
+        public Student(string? name, int[] marks, string grade)
+        {
+            this.Name = name;
+            this.Marks = marks;
+            this.Grade = grade;
+        }
+
+        public string? Name { get => name; set => name = value; }
+        public int[] Marks { get => marks; set => marks = value; }
+        public string Grade { get => grade; set => grade = value; }
+
+        public double CalculateAverage()
+        {
+            double average = Marks.Average();
+            return average;
+        }
+
+        public void DisplayStudentInfo()
+        {
+            Console.WriteLine("Student Name : " + Name);
+            Console.WriteLine("Grade :" + Grade);
+            int i = 1;
+            int total = 0;
+            foreach (int mark in Marks) 
+            {
+                Console.WriteLine("Marks{0}: {1} ", i++, mark);
+                total += mark;
+            }
+            Console.WriteLine("Total marks : " + total);
+            Console.WriteLine();
+        }
+        public void GetMarksSummary()
+        {
+            int high = marks[0];
+            int low = marks[0];
+            foreach (int mark in Marks)
+            {
+                if(mark > high)
+                    high = mark;
+                if(mark < low) low = mark;
+            }
+            Console.WriteLine("Highest :" + high);
+            Console.WriteLine("Lowest : " + low);
+            Console.WriteLine();
+        }
+
+        /*public Student(string name, int grade1, int grade2, int grade3)
         {
             this.name = name;
             this.grade1 = grade1;
@@ -24,6 +73,6 @@ namespace Assignment
             avg = (grade1 + grade2 + grade3)/3;
             return avg;
         }
-
+        */
     }
 }
